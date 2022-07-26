b89b32b39a99861d1bad2e844122adf9b2e60628
Priscila <priscilasartori2@gmail.com>
2022-07-26 15:13:46 -0300

    Resolução do requisito 2

--- src/challenges.js
+++ src/challenges.js
@@ -8,8 +8,9 @@ function compareTrue(animal1, animal2) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let area = (base * height) / 2;
+  return area
 }
 
 // Desafio 3
