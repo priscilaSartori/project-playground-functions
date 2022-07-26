60c4441ca07f017e771a70d9719ea23cc6a66d9d
Priscila <priscilasartori2@gmail.com>
2022-07-26 18:42:52 -0300

    Resolução do requisito 7

--- src/challenges.js
+++ src/challenges.js
@@ -47,9 +47,23 @@ function highestCount(numeros) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
-}
+function catAndMouse(mouse, cat1, cat2) {
+  let gato1 = cat1 - mouse;
+  if (gato1 < 0) {
+    gato1 *= -1;
+  }
+  let gato2 = cat2 - mouse;
+  if (gato2 < 0) {
+    gato2 *= -1;
+  }
+  if (gato1 < gato2) {
+    return 'cat1';
+    } else if (gato2 < gato1) {
+      return 'cat2';
+    } else {
+      return 'os gatos trombam e o rato foge';
+    }
+  }
 
 // Desafio 8
 function fizzBuzz() {
