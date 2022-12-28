be6a9c780aac88eb4efec7cbc62d63decc97ba80
Priscila <priscilasartori2@gmail.com>
2022-12-28 18:13:14 -0300

    subindo projeto

---
+++ src/challenges.js
@@ -0,0 +1,160 @@
+// Desafio 1
+function compareTrue(animal1, animal2) {
+  if (animal1 === true && animal2 === true) {
+    return true;
+  } return false;
+}
+
+// Desafio 2
+function calcArea(base, height) {
+  let area = (base * height) / 2;
+  return area;
+}
+
+// Desafio 3
+function splitSentence(phrase) {
+  let sentence = phrase.split(' ');
+  return sentence;
+}
+
+// Desafio 4
+function concatName(array) {
+  let concatenado = array[array.length -1] + ', ' + array[0];
+  return concatenado;
+}
+
+// Desafio 5
+function footballPoints(wins, ties) {
+  let pontuacao = (wins * 3) + (ties * 1);
+  return pontuacao
+}
+
+// Desafio 6
+function highestCount(numeros) {
+  let maiorNumero = -10;
+  let contNumero = 0;
+  for (let index = 0; index < numeros.length; index += 1) {
+      if (numeros[index] > maiorNumero) {
+      maiorNumero = numeros[index];
+      }
+    }
+    for (let index2 = 0; index2 <= numeros.length; index2 += 1) {
+        if (numeros[index2] === maiorNumero){
+        contNumero += 1;
+        }
+    } 
+  return contNumero;
+}
+
+// Desafio 7
+function catAndMouse(mouse, cat1, cat2) {
+  let gato1 = cat1 - mouse;
+  if (gato1 < 0) {
+    gato1 *= -1;
+  }
+  let gato2 = cat2 - mouse;
+  if (gato2 < 0) {
+    gato2 *= -1;
+  }
+  if (gato1 < gato2) {
+    return 'cat1';
+    } else if (gato2 < gato1) {
+      return 'cat2';
+    } else {
+      return 'os gatos trombam e o rato foge';
+    }
+  }
+
+// Desafio 8
+function fizzBuzz(numeros) {
+  let div = [];
+  for (let index = 0; index <= numeros.length-1; index += 1) {
+    if (numeros[index] % 3 === 0 && numeros[index] % 5 === 0) {
+      div.push('fizzBuzz');
+    } else if (numeros[index] % 3 === 0) {
+      div.push('fizz');
+    } else if (numeros[index] % 5 === 0) {
+      div.push('buzz');
+    } else {
+      div.push('bug!');
+    }
+  } return div;
+}
+
+// Desafio 9
+function encode(phrase) {
+  let frase1 = [""];
+  let frase2 = ' ';
+  for (let letter of phrase) {
+    if (letter === 'a') {
+      frase1.push('1');
+    } else if (letter === 'e') {
+      frase1.push('2');
+    } else if (letter === 'i') {
+      frase1.push('3');
+    } else if (letter === 'o') {
+      frase1.push('4');
+    } else if (letter === 'u') {
+      frase1.push('5');
+    } else {
+      frase1.push(letter);
+    }
+  }
+  frase2 = frase1.join('');
+  return frase2;
+}
+
+function decode(frase2) {
+  let frase3 = [""];
+  let frase4 = "";
+  for (let letter1 of frase2) {
+    if (letter1 === '1') {
+      frase3.push('a');
+    } else if (letter1 === '2') {
+      frase3.push('e');
+    } else if (letter1 === '3') {
+      frase3.push('i');
+    } else if (letter1 === '4') {
+      frase3.push('o');
+    } else if (letter1 === '5') {
+      frase3.push('u');
+    } else {
+      frase3.push(letter1);
+    }
+  }
+  frase4 = frase3.join('');
+  return frase4;
+}
+
+// Desafio 10
+function techList(array, string) {
+  if (array == 0) {
+    return "Vazio!";
+  } else {
+    let tecnologia2 = [];
+    let array2 = array.sort();
+    for (let tec in array2) {
+      let tecnologia = 
+      {
+      "name": string,
+      "tech": array[tec],  
+    }
+    tecnologia2.push(tecnologia);
+  }          
+  return tecnologia2;
+}
+}
+
+module.exports = {
+  calcArea,
+  catAndMouse,
+  compareTrue,
+  concatName,
+  decode,
+  encode,
+  fizzBuzz,
+  footballPoints,
+  highestCount,
+  splitSentence,
+  techList,
+}
---
+++ src/challenges2.js
@@ -0,0 +1,82 @@
+// Desafio 11
+function generatePhoneNumber(numeros) {
+  let repetido = [];
+  let contNumero = 0;
+  let telefone = '(' + numeros[0]  + numeros[1] + ') ' + numeros[2] + numeros[3] + numeros[4] + numeros[5] + numeros[6] + '-' + numeros[7] + numeros[8] + numeros[9] + numeros[10];
+  let test2;
+
+  for (let index = 0; index <= numeros.length; index += 1){
+    if (numeros[index] < 0 || numeros[index] > 9) {
+    test2 = 'menor';
+    }
+  }  
+
+  for (let index2 = 0; index2 <= numeros.length; index2 += 1){
+    for (let index3 = 0; index3 <= numeros.length; index3 += 1){
+      if (numeros[index2] === numeros[index3]) {
+        contNumero += 1;
+        }
+    }
+      if (contNumero >= 3){
+        repetido.push(numeros[index2]);
+      }
+      contNumero = 0;
+  } 
+
+  if (numeros.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  } else if (test2 === 'menor') {
+    return 'não é possível gerar um número de telefone com esses valores';
+  } else if (repetido.length !== 0 ) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  } else {
+    return telefone;
+  }
+}
+
+// Desafio 12
+function triangleCheck(lineA, lineB, LineC) {
+  let soma;
+  let diferenca;
+  if (lineA < lineB + LineC && lineB < lineA + LineC && LineC < lineA + lineB) {
+    soma = 'sim';
+  } else {
+    soma = 'não';
+  }
+
+  if (lineA > Math.abs(lineB - LineC) && lineB > Math.abs(lineA - LineC) && LineC > Math.abs(lineB - lineA)) {
+    diferenca = "sim";
+  } else {
+    diferenca = "não";
+  }
+
+  if (soma === "sim" && diferenca === "sim"){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Desafio 13
+function hydrate(string) {
+let reg = /\d+/g;
+let result = string.match(reg);
+let copo = 0;
+for (let index = 0; index < result.length; index += 1){
+  copo += parseInt(result[index]); 
+}
+
+if (copo === 1) {
+  return copo + ' copo de água';
+} else {
+  return copo + ' copos de água';
+  }
+}
+
+
+
+  module.exports = {
+  generatePhoneNumber,
+  hydrate,
+  triangleCheck,
+};
